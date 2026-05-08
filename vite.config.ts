2734eb491daed6f3fd18987aed26415f815971b8
JPool11 <jpool.1111.2000@gmail.com>
2026-05-07 21:31:13 -0500

    Configuraciones faltantes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/landing_hotel_el_potrillo/",
+  base: "/",
 
   server: {
     host: "::",
